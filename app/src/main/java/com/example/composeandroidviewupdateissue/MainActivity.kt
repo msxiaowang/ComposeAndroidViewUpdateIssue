75c094663aded24f5a653389f3637eccc35ee754
xiaowang <xiaowang@microsoft.com>
2024-04-01 20:17:38 +0800

    fix

--- app/src/main/java/com/example/composeandroidviewupdateissue/MainActivity.kt
+++ app/src/main/java/com/example/composeandroidviewupdateissue/MainActivity.kt
@@ -7,18 +7,23 @@ import android.widget.TextView
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.animateScrollBy
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import kotlinx.coroutines.delay
@@ -27,18 +32,25 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            val state = rememberLazyListState()
+            LaunchedEffect(Unit) {
+                // "Updated string!" shows
+                delay(2000)
+                // Scroll to make AndroidView invisbile
+                state.scrollToItem(1, 200)
+                // Mock the user behavior that scrolls slowly
+                while (state.canScrollBackward) {
+                    state.animateScrollBy(-50f)
+                    delay(50)
+                }
+                // Observed that "Updated string!" is not showing
+            }
             LazyColumn(
                 modifier = Modifier
                     .fillMaxSize()
                     .background(MaterialTheme.colorScheme.background),
+                state = state,
             ) {
-                item {
-                    Box(modifier = Modifier
-                        .fillMaxWidth()
-                        .background(MaterialTheme.colorScheme.primary)
-                        .height(1000.dp)) {
-                    }
-                }
                 item {
                     var message: String? by remember { mutableStateOf(null) }
                     LaunchedEffect(Unit) {
@@ -72,6 +84,21 @@ class MainActivity : AppCompatActivity() {
                             )
                         }
                     )
+                }
+
+                item {
+                    Box(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .background(MaterialTheme.colorScheme.primary)
+                            .height(1000.dp),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Text(
+                            text ="Scrolling",
+                            color = Color.White
+                        )
+                    }
                 }
 
             }
